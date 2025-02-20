c77049c3fa7bcafd58e9d78c60b2c0485fd05254
Rajanandinee Rout <rajanandineerout6370@gmail.com>
2025-02-20 18:04:50 +0530

    Hero and navbar

---
+++ port.js
@@ -0,0 +1,7 @@
+var typed= new typed(".text",{
+    strings:["Student","Frontend Developer","Explorer","Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 80,
+    backdelay: 1000,
+    loop:true
+})
