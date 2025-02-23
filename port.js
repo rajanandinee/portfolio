37ab61040e0c2f7e49cd6cc793a790842e709483
Rajanandinee Rout <rajanandineerout6370@gmail.com>
2025-02-23 18:10:58 +0530

    Refactoring

--- port.js
+++
@@ -1,7 +0,0 @@
-var typed= new typed(".text",{
-    strings:["Student","Desiginer","Frontend Developer","Explorer","Web Developer"],
-    typeSpeed:100,
-    backSpeed:80,
-    backdelay:1000,
-    loop:true
-})
---
+++ script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["Student","Designer","Frontend Developer","Explorer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:80,
+    backdelay:1000,
+    loop:true
+})
