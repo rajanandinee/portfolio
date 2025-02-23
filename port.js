903044ac626742fbd6d81421734b7e31761c108c
Rajanandinee Rout <rajanandineerout6370@gmail.com>
2025-02-23 17:56:03 +0530

    Image added

--- port.js
+++ port.js
@@ -1,7 +1,7 @@
 var typed= new typed(".text",{
-    strings:["Student","Frontend Developer","Explorer","Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 80,
-    backdelay: 1000,
+    strings:["Student","Desiginer","Frontend Developer","Explorer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:80,
+    backdelay:1000,
     loop:true
 })
